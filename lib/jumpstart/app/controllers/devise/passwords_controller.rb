b15900f15499d6b01886e6576b88538f20b55121
Chris Oliver <excid3@gmail.com>
2025-10-10 15:44:49 -0500

    Lint devise controller

--- lib/jumpstart/app/controllers/devise/passwords_controller.rb
+++ lib/jumpstart/app/controllers/devise/passwords_controller.rb
@@ -55,37 +55,38 @@ class Devise::PasswordsController < DeviseController
   end
 
   protected
-    def sign_in_after_reset_password?(resource)
-      setting = resource_class.sign_in_after_reset_password
-      setting.respond_to?(:call) ? setting.call(resource) : setting
-    end
 
-    def after_resetting_password_path_for(resource)
-      sign_in_after_reset_password?(resource) ? after_sign_in_path_for(resource) : new_session_path(resource_name)
-    end
+  def sign_in_after_reset_password?(resource)
+    setting = resource_class.sign_in_after_reset_password
+    setting.respond_to?(:call) ? setting.call(resource) : setting
+  end
 
-    # The path used after sending reset password instructions
-    def after_sending_reset_password_instructions_path_for(resource_name)
-      new_session_path(resource_name) if is_navigational_format?
-    end
+  def after_resetting_password_path_for(resource)
+    sign_in_after_reset_password?(resource) ? after_sign_in_path_for(resource) : new_session_path(resource_name)
+  end
 
-    # Check if a reset_password_token is provided in the request
-    def assert_reset_token_passed
-      if params[:reset_password_token].blank?
-        set_flash_message(:alert, :no_token)
-        redirect_to new_session_path(resource_name)
-      end
-    end
+  # The path used after sending reset password instructions
+  def after_sending_reset_password_instructions_path_for(resource_name)
+    new_session_path(resource_name) if is_navigational_format?
+  end
 
-    # Check if proper Lockable module methods are present & unlock strategy
-    # allows to unlock resource on password reset
-    def unlockable?(resource)
-      resource.respond_to?(:unlock_access!) &&
-        resource.respond_to?(:unlock_strategy_enabled?) &&
-        resource.unlock_strategy_enabled?(:email)
+  # Check if a reset_password_token is provided in the request
+  def assert_reset_token_passed
+    if params[:reset_password_token].blank?
+      set_flash_message(:alert, :no_token)
+      redirect_to new_session_path(resource_name)
     end
+  end
 
-    def translation_scope
-      'devise.passwords'
-    end
+  # Check if proper Lockable module methods are present & unlock strategy
+  # allows to unlock resource on password reset
+  def unlockable?(resource)
+    resource.respond_to?(:unlock_access!) &&
+      resource.respond_to?(:unlock_strategy_enabled?) &&
+      resource.unlock_strategy_enabled?(:email)
+  end
+
+  def translation_scope
+    "devise.passwords"
+  end
 end
